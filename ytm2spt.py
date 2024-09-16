a7ce9247bc02c78df0efd05df524f2f90ec040e2
Abhishek M J <abhishekmj303@gmail.com>
2024-09-16 16:34:42 +0530

    Add windows installer build
    Fix build dir and var name
    
    Fix onefile in build.yaml

--- ytm2spt.py
+++ ytm2spt.py
@@ -1,6 +1,12 @@
+# Nuitka Configuration
+# nuitka-project: --enable-plugins=pyside6
+# nuitka-project: --user-package-configuration-file=ytmusicapi.nuitka-package.config.yaml
+#
 # Compilation mode, standalone everywhere, except on macOS there app bundle
-# nuitka-project-if: {OS} in ("Windows", "Linux", "FreeBSD"):
+# nuitka-project-if: {OS} in ("Linux", "FreeBSD"):
 #    nuitka-project: --onefile
+# nuitka-project-if: {OS} == "Windows":
+#    nuitka-project: --standalone
 # nuitka-project-if: {OS} == "Darwin":
 #    nuitka-project: --standalone
 #    nuitka-project: --macos-create-app-bundle
