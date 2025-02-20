91e1edd8b353186aa0d274a7f2b7fcb02560d33a
Abhishek M J <abhishekmj303@gmail.com>
2025-02-20 19:12:45 +0530

    Fix build error with nuitka

--- ytm2spt.py
+++ ytm2spt.py
@@ -2,15 +2,6 @@
 # nuitka-project: --enable-plugins=pyside6
 # nuitka-project: --user-package-configuration-file=ytmusicapi.nuitka-package.config.yaml
 #
-# Compilation mode, standalone everywhere, except on macOS there app bundle
-# nuitka-project-if: {OS} in ("Linux", "FreeBSD"):
-#    nuitka-project: --onefile
-# nuitka-project-if: {OS} == "Windows":
-#    nuitka-project: --standalone
-# nuitka-project-if: {OS} == "Darwin":
-#    nuitka-project: --standalone
-#    nuitka-project: --macos-create-app-bundle
-#
 # Debugging options, controlled via environment variable at compile time.
 # nuitka-project-if: os.getenv("DEBUG_COMPILATION", "yes") == "yes":
 #     nuitka-project: --enable-console
