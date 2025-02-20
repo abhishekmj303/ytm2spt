5ddfdb05048943760d9046ee9faef395526c4ad8
Abhishek M J <abhishekmj303@gmail.com>
2025-02-20 18:50:36 +0530

    remove all comment

--- ytm2spt.py
+++ ytm2spt.py
@@ -2,12 +2,6 @@
 # nuitka-project: --enable-plugins=pyside6
 # nuitka-project: --user-package-configuration-file=ytmusicapi.nuitka-package.config.yaml
 #
-# Compilation mode, standalone everywhere, except on macOS there app bundle
-# nuitka-project-if: {OS} in ("Linux", "FreeBSD"):
-#    nuitka-project: --onefile
-# nuitka-project-if: {OS} == "Windows":
-#    nuitka-project: --standalone
-#
 # Debugging options, controlled via environment variable at compile time.
 # nuitka-project-if: os.getenv("DEBUG_COMPILATION", "yes") == "yes":
 #     nuitka-project: --enable-console
